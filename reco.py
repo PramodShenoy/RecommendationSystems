6e01a24c2b661f5861eb7ebad60dc69ec6fd2021
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-03-04 01:10:09 +0530

    tried euclidean scoring

---
+++ reco.py
@@ -0,0 +1,38 @@
+from math import sqrt,pow
+# mock dataset for movie reviews
+
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
+'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
+'You, Me and Dupree': 3.5},
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+'The Night Listener': 4.5, 'Superman Returns': 4.0,
+'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+'You, Me and Dupree': 2.0},
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+def euclidean_dist(p1,p2):
+	si={}
+	print 'sdfsdf'
+	#finding common ratings
+	for item in critics[p1]:
+		if item in critics[p2]:
+			si[item]=1
+
+	#no ratings in common
+	if(len(si)==0):
+		return 0
+	
+	func=sum([pow(critics[p1][item]-critics[p2][item],2) for item in critics[p1] if item in critics[p2]])
+	return 1/(1+func)
+	
+
+print euclidean_dist('Lisa Rose','Jack Matthews')
